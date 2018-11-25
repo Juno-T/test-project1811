d2d9332b4ef54f1c6e8682124daf386635e6e908
plowshare18 <onuj1234@gmail.com>
2018-11-25 16:19:56 +0900

    spelling debug in setting.py

--- airline0/airline/settings.py
+++ airline0/airline/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'airline.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=false)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=False)
 
 
 # Password validation
